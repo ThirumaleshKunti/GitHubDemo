8192d245209a4a7500ff17a3a5b130a4303ce5d2
ThirumaleshKunti <kunti.thirumalesh@gmail.com>
2021-06-03 12:37:15 +0530

    added GitHubDemo project to local repo

---
+++ src/main/java/com/hcl/gitdemo/Demo.java
@@ -0,0 +1,11 @@
+package com.hcl.gitdemo;
+
+public class Demo {
+	public void getData(String data) {
+		System.out.println("data : " + data);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello wold...");
+	}
+}
