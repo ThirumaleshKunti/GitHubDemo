9cf0bada4f1b2fe3d8800b871e8260c6aa88bb27
ThirumaleshKunti <kunti.thirumalesh@gmail.com>
2021-06-03 12:43:44 +0530

    added if condition to check data

--- src/main/java/com/hcl/gitdemo/Demo.java
+++ src/main/java/com/hcl/gitdemo/Demo.java
@@ -3,6 +3,9 @@ package com.hcl.gitdemo;
 public class Demo {
 	public void getData(String data) {
 		System.out.println("data : " + data);
+		if (data.equals("Thirumalesh")) {
+			System.out.println("Welcome to Github local repo...");
+		}
 	}
 
 	public static void main(String[] args) {
