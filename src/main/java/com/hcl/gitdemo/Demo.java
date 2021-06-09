76ece9f80e4e36a88e7894d0c65525b648503bde
ThirumaleshKunti <kunti.thirumalesh@gmail.com>
2021-06-09 16:49:59 +0530

    added else condition and done the changes in main method.

--- src/main/java/com/hcl/gitdemo/Demo.java
+++ src/main/java/com/hcl/gitdemo/Demo.java
@@ -1,15 +1,18 @@
 package com.hcl.gitdemo;
 
 public class Demo {
-	public void getData(String data) {
+	public static void getData(String data) {
 		System.out.println("data : " + data);
 		if (data.equals("Thirumalesh")) {
 			System.out.println("Welcome to Github local repo...");
+		} else if (data.equals("Thiru")) {
+			System.out.println("Code moved from local repo to git repo...");
 		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Hello wold...");
+		getData("Thiru");
 	}
-	
+
 }
